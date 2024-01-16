3c0c31a979927e33b2523a61061014962e240080
d-shetty33 <dhanyashetty332@gmail.com>
2024-01-16 11:45:43 +0530

    Done By Dhanya

--- src/Routes/Company/Contact/Contact.jsx
+++ src/Routes/Company/Contact/Contact.jsx
@@ -24,8 +24,8 @@ const Contact = () => {
     // }
     console.log("hello")
     const res= await database.createDocument("658fb4872811d10690cc","658fb50b21159a9e22ba",ID.unique(),{
-      body:message,
-      username:name,email
+      desc:name,email,
+      username:message
     })
     setName("") 
     setEmail("")
